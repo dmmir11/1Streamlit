12c9c45300f7372c9ab9657de088798b19414489
Diana <dmmir2020@gmail.com>
2022-04-27 02:29:56 +0200

    upd map, errors

--- streamlit.py
+++ streamlit.py
@@ -27,21 +27,27 @@ if st.sidebar.button("Get the last information of all countries"):
 # Australia
 australia_df = pd.read_csv('Australia.csv')
 australia_df["Country"] = "Australia"
+australia_df["countryterritoryCode"] = "AU"
 # Austria
 austria_df = pd.read_csv('Austria.csv')
 austria_df["Country"] = "Austria"
+austria_df["countryterritoryCode"] = "AT"
 # Brazil
 brazil_df = pd.read_csv('Brazil.csv')
 brazil_df["Country"] = "Brazil"
+brazil_df["countryterritoryCode"] = "BR"
 # Germany
 germany_df = pd.read_csv('Germany.csv')
 germany_df["Country"] = "Germany"
+germany_df["countryterritoryCode"] = "DE"
 # Russia
 russia_df = pd.read_csv('Russia.csv')
 russia_df["Country"] = "Russia"
+russia_df["countryterritoryCode"] = "RU"
 # USA
 usa_df = pd.read_csv('USA.csv')
 usa_df["Country"] = "USA"
+usa_df["countryterritoryCode"] = "US"
 
 data_all = [australia_df, austria_df, brazil_df, germany_df, usa_df, russia_df]
 df_all = pd.concat(data_all, ignore_index=True)
@@ -64,6 +70,7 @@ st.sidebar.markdown("### Visualisation")
 select = st.sidebar.selectbox('Visualisation type', ['Chose an option', 'Line Graph', 'Bar Chart', 'Map'], key='1')
 # sentiment_count = data['airline_sentiment'].value_counts()
 # sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
+
 
 
 # if select == "Line Graph":
@@ -135,3 +142,42 @@ if select == "Bar Chart":
 
         fig = px.bar(result_df, x='Year', y=indicator_choice[0], color="Country", height=500)
         st.plotly_chart(fig)
+
+
+
+# GDP карта без цвета, inflation and unemployment ERROR 
+if select == "Map":
+    st.sidebar.subheader("What time period are you interested in?")
+    year = st.sidebar.slider(
+        'Select a range of years',
+        1999, 2021, (2001, 2005))
+    st.sidebar.write('Chosen:', year)
+    st.sidebar.subheader("Select countries")
+    choice = st.sidebar.multiselect('Pick countries', ('Australia', 'Austria', 'Brazil', 'Germany', 'Russia', 'USA'),
+                                    key='0')
+    st.sidebar.subheader("Select indicators")
+    indicator_choice = st.sidebar.multiselect('Pick indicators', ('GDP', 'Inflation rate', 'Unemployment rate'),
+                                              key='0')
+
+    if len(indicator_choice) > 0 and len(choice) > 0:
+        result_df = df_all[(df_all.Country.isin(choice)) & (df_all["Year"] >= year[0]) & (df_all["Year"] <= year[1])][
+            [indicator_choice[0], "Year", "Country"]]
+
+        
+        fig = px.choropleth(df_all, locations="countryterritoryCode",
+                    color=indicator_choice[0],
+                    hover_name="Country",
+                    animation_frame="Year",
+                    color_continuous_scale="Sunsetdark",
+                    projection = 'equirectangular')
+        st.plotly_chart(fig)
+
+
+# fig.update_geos(fitbounds="locations")
+# fig.update_layout(margin={'r':0,'t':50,'l':0,'b':0})
+# fig.show()
+
+# fig = px.line(df[filter1], x="Years", y=indicator_choice[0], color="Country")
+# fig.show()
+        
+        
